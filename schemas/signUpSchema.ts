0869beac459ccfd519e8e409433415cec774a50f
Ro706 <rohitmandal99091@gmail.com>
2025-12-19 20:43:17 +0530

    update [19-12-2025] Add checking system to check username is unique or not and also created route for verify code (OTP)

--- schemas/signUpSchema.ts
+++ schemas/signUpSchema.ts
@@ -1,12 +1,12 @@
 import {z} from 'zod'
 
-export const USernameValidation = z.string()
+export const UsernameValidation = z.string()
     .min(2, {message: 'Username must be at least 3 characters long'})
     .max(30, {message: 'Username must be at most 30 characters long'})
     .regex(/^[a-zA-Z0-9_]+$/, {message: 'Username can only contain letters, numbers, and underscores'})
 
 export const signUpSchema = z.object({
-    username: USernameValidation,
+    username: UsernameValidation,
     email: z.string().
         email({message: 'Invalid email address'}),
     password: z.string()
